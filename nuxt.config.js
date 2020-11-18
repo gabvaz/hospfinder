c837202e506653ea37f133eaf064a925d1b0fc8e
Gabriel Vaz <gabriel.lemos@melhorcom.com.br>
2020-11-17 21:16:19 -0300

    Page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   target: "static",
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Hospfinder',
+    title: 'HospFinder',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
